cd20cb1685fe5d55e13470ac6155370a56027359
Anton Dementyev <antdementyev@gmail.com>
2018-07-22 00:52:02 +0300

    ke5: convert projects to maven

--- WebApplication2/src/main/java/unihagen/web/servlets/MyServlet.java
+++ WebApplication2/src/main/java/unihagen/web/servlets/MyServlet.java
@@ -1,13 +1,12 @@
 package unihagen.web.servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
 
 @WebServlet(name="MyServlet", urlPatterns={"/bla"})
 public class MyServlet extends HttpServlet {
